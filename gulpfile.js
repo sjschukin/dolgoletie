ae6c602225770f164928deba29f3000285deb369
Sergey Shchukin <s.j.schukin@yandex.ru>
2023-05-27 15:22:37 +0300

    fixed gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,15 @@
 const { src, dest, parallel, series, watch } = require('gulp');
-const browserSync = require('browser-sync').create();
+const browsersync = require('browser-sync').create();
 const sass = require('gulp-sass')(require('sass'));
 const sourcemaps = require('gulp-sourcemaps');
 
-const themeRoot = "web/themes/custom/discreet_tan/";
+const themeRoot = 'web/themes/custom/discreet_tan/';
 
-function browsersync() {
-  browserSync.init({
-    proxy: "dolgoletie.ryazanszn.test"
+function browsersyncServe(cb) {
+  browsersync.init({
+    proxy: 'dolgoletie.ryazanszn.test'
   });
+  cb();
 }
 
 function styles() {
@@ -18,21 +19,26 @@ function styles() {
     .on('error', sass.logError)
     .pipe(sourcemaps.write('./'))
     .pipe(dest(themeRoot + 'css'))
-    .pipe(browserSync.stream());
+    .pipe(browsersync.stream());
 }
 
-function startwatch() {
+function browsersyncReload(cb) {
+  browsersync.reload();
+  cb();
+}
+
+function watchTask() {
   let files = [
-    themeRoot + "css/*.css",
-    themeRoot + "js/*.js",
-    themeRoot + "images/**/*",
-    themeRoot + "templates/**/*.twig"
+    themeRoot + 'scss/*.scss',
+    themeRoot + 'js/*.js',
+    themeRoot + 'images/**/*',
+    themeRoot + 'templates/**/*.twig'
   ];
 
-  watch(files, browserSync.reload);
+  watch(files, series(styles, browsersyncReload));
 }
 
 exports.browsersync = browsersync;
 exports.styles = styles;
 exports.build = series(styles);
-exports.default = parallel(styles, browsersync, startwatch);
+exports.default = series(styles, browsersyncServe, watchTask);
