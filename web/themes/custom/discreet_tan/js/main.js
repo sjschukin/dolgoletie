84e807dc9af6dc08db4266fcb9c53cb41f9d7502
Sergey Shchukin <s.j.schukin@yandex.ru>
2023-05-28 21:41:41 +0300

    updated theme

---
+++ web/themes/custom/discreet_tan/js/main.js
@@ -0,0 +1,20 @@
+/**
+ * @file
+ */
+
+(function () {
+  "use strict";
+
+  // Sticky
+  window.onscroll = function () {
+    var header_navbar = document.getElementById("header_navbar");
+    var sticky = header_navbar.offsetTop;
+
+    if (window.pageYOffset > sticky) {
+        header_navbar.classList.add("sticky");
+    } else {
+        header_navbar.classList.remove("sticky");
+    }
+  };
+
+})();
