3a837c64fa866d3f25f1d017c75b63feb7a1e374
Sergey Shchukin <s.j.schukin@yandex.ru>
2023-05-31 23:12:08 +0300

    added back-to-top button

--- web/themes/custom/discreet_tan/js/front-page.js
+++ web/themes/custom/discreet_tan/js/front-page.js
@@ -5,15 +5,24 @@
 (function () {
   "use strict";
 
-  // Sticky
   window.onscroll = function () {
-    var header_navbar = document.getElementById("header_navbar");
-    var sticky = header_navbar.offsetTop;
+    // Sticky
+    let header_navbar = document.getElementById("header_navbar");
+    let sticky = header_navbar.offsetTop;
 
     if (window.pageYOffset > sticky) {
         header_navbar.classList.add("sticky");
     } else {
         header_navbar.classList.remove("sticky");
+    }
+
+    // Back to top button
+    let backToTop = document.querySelector(".back-to-top");
+
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+      backToTop.style.display = "block";
+    } else {
+      backToTop.style.display = "none";
     }
   };
 
