76fae2dcbe2fb59d5cb9d0358450342f02dcc2dd
Sergey Shchukin <s.j.schukin@yandex.ru>
2025-08-11 23:34:56 +0300

    feat: add offcanvas menu

--- web/themes/custom/discreet_tan/js/main.js
+++ web/themes/custom/discreet_tan/js/main.js
@@ -5,4 +5,12 @@
 (function () {
   "use strict";
 
+  document.querySelectorAll('.nav-link').forEach(item => {
+    item.addEventListener('click', () => {
+      // Close the offcanvas menu
+      var bsOffcanvas = bootstrap.Offcanvas.getInstance(document.getElementById('offcanvasNavbar'));
+      bsOffcanvas.hide();
+    });
+  });
+
 })();
